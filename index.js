0163137aaf34bfc089f6454ce458ead586b74156
PrinceSP <princepasombaran@gmail.com>
2020-12-17 19:36:25 +0800

    make add function to add list, and complete function for line-through text

---
+++ index.js
@@ -0,0 +1,38 @@
+const ul = document.querySelector('ul')
+const forms = document.querySelector('form')
+const btnAdd =  document.querySelector('.adds')
+const inputs = document.querySelector('.inputs')
+
+forms.addEventListener('submit',add)
+
+function add(e){
+  e.preventDefault()
+  const lists = document.createElement('li')
+  const box = document.createElement('input')
+
+  box.type = "checkbox"
+  box.className = 'done'
+
+  if (inputs.value == '') {
+    alert('input box must not be empty!')
+    return false;
+  }
+
+  lists.appendChild(box)
+  lists.append(inputs.value)
+
+  ul.appendChild(lists)
+
+  inputs.value = ''
+
+  box.addEventListener('change',function(){
+    complete(this,lists)
+  })
+
+}
+
+function complete(e,text){
+  if (e.checked==true) {
+    text.style.textDecoration = 'line-through'
+  }
+}
